f4e729cbb7bdab431c9b839d01ab71699dff006a
Danil Kalashnikov <scrolltex@gmail.com>
2018-04-16 18:48:16 +0700

    Add edit & delete section feature

--- lib/pages/sections_page.dart
+++ lib/pages/sections_page.dart
@@ -12,8 +12,10 @@ class SectionsViewModel {
   final List<SectionModel> sections;
   final List<TodoEntryModel> todos;
   final Function(SectionModel) addSectionCallback;
+  final Function(SectionModel) updateSectionCallback;
+  final Function(SectionModel) deleteSectionCallback;
 
-  SectionsViewModel({this.sections, this.todos, this.addSectionCallback});
+  SectionsViewModel({this.sections, this.todos, this.addSectionCallback, this.updateSectionCallback, this.deleteSectionCallback});
 }
 
 class SectionsPage extends StatelessWidget {
@@ -26,7 +28,12 @@ class SectionsPage extends StatelessWidget {
           sections: store.state.sections,
           todos: store.state.todos,
           addSectionCallback: (section) =>
-              store.dispatch(new AddSectionAction(section))),
+              store.dispatch(new AddSectionAction(section)),
+          updateSectionCallback: (section) =>
+              store.dispatch(new UpdateSectionAction(section)),
+          deleteSectionCallback: (section) =>
+              store.dispatch(new DeleteSectionAction(section))
+      ),                      
       builder: (context, viewModel) {
         return new Scaffold(
           appBar: new AppBar(
@@ -34,8 +41,7 @@ class SectionsPage extends StatelessWidget {
           ),
           body: _buildSectionsList(context, viewModel),
           floatingActionButton: new FloatingActionButton(
-            onPressed: () => _addNewSection(
-                context, (section) => viewModel.addSectionCallback(section)),
+            onPressed: () => _addNewSection(context, viewModel),
             tooltip: 'Add new section',
             child: new Icon(Icons.add),
           ),
@@ -55,6 +61,7 @@ class SectionsPage extends StatelessWidget {
       return new InkWell(
         onTap: () => Navigator.of(context).push(new MaterialPageRoute(
           builder: (context) => new TodoListPage(entry))),
+        onLongPress: () => _showActionsMenu(context, viewModel, entry),//_editSection(context, viewModel.updateSectionCallback, entry),
         child: new ListTile(
           title: new Text('${entry.title} (${doneTodosInSection.length}/${todosInSection.length})'),
           trailing: new Icon(Icons.arrow_right),
@@ -72,12 +79,48 @@ class SectionsPage extends StatelessWidget {
         : new Center(child: new Text("No sections", style: textStyle));
   }
 
-  void _addNewSection(BuildContext context, Function(SectionModel) addSectionCallback) async {
+  void _showActionsMenu(BuildContext context, SectionsViewModel viewModel, SectionModel section) async {
+    var isEditOrDelete = await showDialog<bool>(
+      context: context,
+      builder: (context) => new SimpleDialog(
+        children: <Widget>[
+          new ListTile(
+            leading: new Icon(Icons.edit),
+            title: new Text("Edit"),
+            onTap: () => Navigator.of(context).pop(true)
+          ),
+          new ListTile(
+            leading: new Icon(Icons.delete),
+            title: new Text("Delete"),
+            onTap: () => Navigator.of(context).pop(false),
+          )
+        ],          
+      )
+    );
+
+    if (isEditOrDelete != null) {
+      if (isEditOrDelete) {
+        _editSection(context, viewModel, section);
+      } else {
+        viewModel.deleteSectionCallback(section);
+      }
+    }
+  }
+
+  void _addNewSection(BuildContext context, SectionsViewModel viewModel) async {
     var title = await showDialog<String>(
         context: context, builder: (context) => new AddSectionDialog());
 
     if (title != null && title.isNotEmpty)
-      addSectionCallback(new SectionModel(id: 0, title: title));
+      viewModel.addSectionCallback(new SectionModel(id: 0, title: title));
+  }
+
+  void _editSection(BuildContext context, SectionsViewModel viewModel, SectionModel section) async {
+    var updated = await showDialog<SectionModel>(
+        context: context, builder: (context) => new EditSectionDialog(section));
+      
+    if (updated != null)
+      viewModel.updateSectionCallback(updated);
   }
 }
 
@@ -100,5 +143,56 @@ class AddSectionDialog extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class EditSectionDialog extends StatefulWidget {
+  final SectionModel section;
+
+  EditSectionDialog(this.section);
+
+  @override
+  EditSectionDialogState createState() => new EditSectionDialogState();
+}
+
+class EditSectionDialogState extends State<EditSectionDialog> {
+  final _formKey = new GlobalKey<FormState>();
+
+  bool _autovalidate = false;
+  bool valid = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Form(
+    key: _formKey,
+    autovalidate: _autovalidate,      
+    child: new AlertDialog(
+      title: new Text("Edit section"),
+      content:  new TextFormField(
+        initialValue: widget.section.title,
+        validator: (str) => str.isEmpty ? "Required" : null,
+        onSaved: (str) => widget.section.title = str,
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          child: new Text("CANCEL"),
+          onPressed: () => Navigator.of(context).pop(null)
+        ),
+        new FlatButton(
+          child: new Text("SAVE"),
+          onPressed: _save
+        )
+      ],
+    ));
+  }
+
+  void _save() {
+    var form = _formKey.currentState;
+    if (!form.validate()) {
+      _autovalidate = true;
+    } else {      
+      form.save();
+      Navigator.of(context).pop(widget.section);
+    }
   }
 }
